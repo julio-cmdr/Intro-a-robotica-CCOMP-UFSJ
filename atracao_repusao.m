fad5af2ba72bcf09b19e95b778583ac287b0d46d
julio-cmdr <julio.cmdr@gmail.com>
2019-08-21 16:22:00 -0300

    refactorando

--- atracao_repusao.m
+++ atracao_repusao.m
@@ -1,78 +1,62 @@
-function [] = aula1()
-  robo(1)=criaobj([35 35],5,[10 10],.5);
-  robo(2)=criaobj([65 65],5,[10 10],0.5);
-  bola=criaobj([80 80],2,[80 80],0.01);
-%  obst=criaobj([20 20],5,[0 0],0);
- 
- robo(1).alvo=bola.local;
-    robo(1).raio_alvo=bola.raio;
-    robo(2).alvo=bola.local;
-    robo(2).raio_alvo=bola.raio;
-    robo(1).kr=-1;
-    robo(2).kr=-1;
- 
+function [] = main()
+  bola = criaObj([80 80], 2);
+  robo(1) = criaRobo([35 35], 5, bola, 0.5, -1);
+  robo(2) = criaRobo([65 65], 5, bola, 0.5, -1);
+
   while (1)
-    
     robo(1)=move(robo(1),robo(2));
     robo(2)=move(robo(2),robo(1));
     
-    
-    
-    plot(robo(1).local(1),robo(1).local(2),'r+', robo(2).local(1),robo(2).local(2),'b+', bola.local(1), bola.local(2), 'mo');
+    plot(robo(1).pos(1),robo(1).pos(2),'r+', robo(2).pos(1),robo(2).pos(2),'b+', bola.pos(1), bola.pos(2), 'mo');
     axis([0 100 0 100]);
     drawnow;  
   end
 end
 
+function [robo] = criaRobo(pos, raio, alvo, k_a, k_r)
+  robo.pos = pos;
+  robo.raio = raio;
+  robo.alvo = alvo;
+  robo.k_a = k_a;      # constante atracao
+  robo.k_r = k_r;      # constante repulsao
+end
 
-function [obj]=criaobj(local,raio,alvo,k_a)
-  obj.local=local; 
-  obj.raio=raio;
-  obj.alvo=alvo;
-  obj.k=k_a;
+function [obj] = criaObj(pos,raio)
+  obj.pos = pos; 
+  obj.raio = raio;
 end
   
-function [obj]=move(obj,obs)
-  robo.x=obj.local(1);
-  robo.y=obj.local(2);
-  alvo.x=obj.alvo(1);
-  alvo.y=obj.alvo(2);
-  r=obj.raio;
-  ra=obj.raio_alvo;
-  k=obj.k;  
-  [robo]=atracao(alvo,robo,k,r,ra);
-  if length(obs)>0
-   kr=obj.kr;
-   obst.x=obs.local(1);
-   obst.y=obs.local(2);
-   ro=obs.raio;
-  [robo]=repulsao(obst,robo,kr,r,ro);
-end
-  obj.local(1)=robo.x;
-  obj.local(2)=robo.y;
+function [robo] = move(robo, obst)
+  [robo] = atracao(robo, robo.alvo, robo.k_a);
+  
+  for i=1:length(obst)
+     [robo] = repulsao(robo, obst(i), robo.k_r);
+  end
 end
 
-function [robo]=atracao(alvo,robo,k,ra,rr)
-  dist.x = alvo.x - robo.x;
-  dist.y = alvo.y - robo.y;
+function [robo] = atracao(robo, alvo, k)
+  dist.x = alvo.pos(1) - robo.pos(1);
+  dist.y = alvo.pos(2) - robo.pos(2);
   dist.e = sqrt(dist.x^2 + dist.y^2);
-  dx=dist.x/dist.e + 0.1*randn;
-  dy=dist.y/dist.e + 0.1*randn;
-  if dist.e>rr+ra
-    robo.x = robo.x + k*dx;
-    robo.y = robo.y + k*dy;
+  
+  dx = dist.x/dist.e + 0.1*randn;
+  dy = dist.y/dist.e + 0.1*randn;
+  
+  if dist.e > robo.raio + alvo.raio
+    robo.pos(1) = robo.pos(1) + k*dx;
+    robo.pos(2) = robo.pos(2) + k*dy;
   end
 end 
 
-function [robo]=repulsao(obst,robo,kr,rr,ro)
-  dist.x = obst.x - robo.x;
-  dist.y = obst.y - robo.y;
+function [robo]=repulsao(robo, obst, k)
+  dist.x = obst.pos(1) - robo.pos(1);
+  dist.y = obst.pos(2) - robo.pos(2);
   dist.e = sqrt(dist.x^2 + dist.y^2);
   dx=dist.x/dist.e + 0.1*randn;
   dy=dist.y/dist.e + 0.1*randn;
 
-  if dist.e.^2<(rr+ro).^2
-    robo.x = robo.x + kr*dx;
-    robo.y = robo.y + kr*dy;
+  if dist.e.^2<(robo.raio + obst.raio).^2
+    robo.pos(1) = robo.pos(1) + k*dx;
+    robo.pos(2) = robo.pos(2) + k*dy;
   end
 end 
