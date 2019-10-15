248113edbf429449f1d789b1ec696e555b39aeb6
julio.cmdr <julio.cmdr@gmail.com>
2019-10-14 23:33:23 -0300

    separação megaman e inimigo

--- megaman.m
+++ megaman.m
@@ -6,7 +6,7 @@ for i=1:100
   hsv = rgb2hsv(rgb);
 
   bin_megaman = hsv(:,:,2) == 1 & hsv(:,:,3) > 0.9 & hsv(:,:,1) < 0.6 & hsv(:,:,1) > 0.1;
-  bin_inimigo = hsv(:,:,1) < 0.2 & hsv(:,:,2) > 0.63 & hsv(:,:,3) > 0.6;
+  bin_inimigo = hsv(:,:,1) < 0.2 & hsv(:,:,2) > 0.63 & hsv(:,:,3) > 0.95;
 
   bin_megaman = imclose(bin_megaman, ones(7));
   bin_inimigo = imclose(bin_inimigo, ones(7));
@@ -18,14 +18,11 @@ for i=1:100
   mega_x = round(coordenadas.Centroid(2));
   mega_y = round(coordenadas.Centroid(1));
 
-  coordenadas = regionprops(bin_inimigo, "Centroid");
-  ini_x = round(coordenadas.Centroid(2));
-  ini_y = round(coordenadas.Centroid(1));
-
-  raio = 15;
+  raio = 20;
 
   drawnow;
+  imshow(bin_inimigo);
   subplot(1,3,1); imshow(rgb);
-  subplot(1,3,2); imshow(rgb_megaman(mega_x-raio:mega_x+raio, mega_y-raio:mega_y+raio, :));
-  subplot(1,3,3); imshow(rgb_megaman(ini_x-raio:ini_x+raio, ini_y-raio:ini_y+raio, :));
+  subplot(1,3,2); imshow(rgb_inimigo .+ rgb_megaman);
+  subplot(1,3,3); imshow(rgb_megaman(mega_x-raio:mega_x+raio, mega_y-raio:mega_y+raio, :));
 endfor;
